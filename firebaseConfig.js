72b646ce9c7f2bdd6fbac612e114560fbc1e04f4
Aditya Navnit Patil <75129750+adityapatil687@users.noreply.github.com>
2022-12-18 18:31:55 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -6,14 +6,14 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD7GohVyaiY1ZIiN1GC_BxbR3QRgxXB5WI",
-  authDomain: "rfidadmin-5d6ca.firebaseapp.com",
-  databaseURL: "https://rfidadmin-5d6ca-default-rtdb.firebaseio.com",
-  projectId: "rfidadmin-5d6ca",
-  storageBucket: "rfidadmin-5d6ca.appspot.com",
-  messagingSenderId: "706414298478",
-  appId: "1:706414298478:web:3573a0b0e3fe0021f0ed7f",
-  measurementId: "G-GCF95SFEB6"
+  apiKey: " ",
+  authDomain: " ",
+  databaseURL: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: " "
 };
 
 // Initialize Firebase
